280e5117b303d03a28ce3adbb02578f2909f6db5
Nick Gashkov <nick@gashkov.com>
2019-02-08 11:52:13 +0300

    Add docs for 'core.Registry'

--- autofactory/core/registry.py
+++ autofactory/core/registry.py
@@ -1,17 +1,31 @@
 try:
     from collections import UserDict
 except ImportError:
+    # PORT: Python 2.
     from UserDict import UserDict
 
 
 class Registry(UserDict):
-    def register(self, key, function=None):
-        if function is not None:
-            # Called as Registry.register('key', function)
-            self.data[key] = function
+    def register(self, key, value=None):
+        """A convenience method to register values.
+
+        Can be used as a function or as a decorator:
+
+            r.register('key', as_a_function)
+
+            @r.register('key')
+            def as_a_decorator():
+                ...
+
+        """
+        if value is not None:
+            self._register(key, value)
             return
 
         def decorator(func):
-            self.data[key] = func
+            self._register(key, func)
 
         return decorator
+
+    def _register(self, key, value):
+        self.data[key] = value
